7ff60b6562697cd82e5684dfcbeccf9c90599c44
Igor Vedernikov <pulmar@bk.ru>
2022-07-09 19:35:59 +0300

    Проверка commit

---
+++ lesson1/task6.py
@@ -0,0 +1,10 @@
+start = int(input("Пробежал 1й день"))
+end = int(input("Необходимый результат"))
+day = 0
+res = start
+while res < end:
+    print(f"{day+1}-й день: {res}")
+    res += res * 0.1
+    day += 1
+print(f"на {day+1}й день спортсмен достиг результата")
+
