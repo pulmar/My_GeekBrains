b0e064c175d9df84513fdbc51100d179e5db228b
Igor Vedernikov <pulmar@bk.ru>
2022-07-09 15:45:36 +0300

    Проверка commit

--- lesson1/task2.py
+++ lesson1/task2.py
@@ -1,4 +1,4 @@
 sec = int(input("Введите время в секундах"))
-h = int (sec / 3600)
-m = int (sec / 60)
-print(f"Время: {h}:{m}:{sec}")
+h = int (sec // 3600)
+m = int (sec // 60)
+print(f"Время: {h}:{m%60}:{sec%60}")
